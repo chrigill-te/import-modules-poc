706ec66d1d937705449b496cf191636baf445d40
Chris Gill <chrigill@thousandeyes.com>
2023-10-19 16:22:15 +0100

    init

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <button :onClick="click">{{ test }}</button>
+  <TeButton :onClick="click">This button is from te-cc</TeButton>
+</template>
+
+<script setup lang="ts">
+// import { TeButton } from "@te/ui-components/dist/base";
+
+import dayJs from "dayjs";
+import { computed } from "vue";
+function click() {
+  console.log("click click");
+}
+
+const test = computed(() => dayJs());
+</script>
